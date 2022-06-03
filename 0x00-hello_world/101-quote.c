276195a4b8be076dc68537ade07ca1b333b1e8dc
sualah <sualahabdullai@gmail.com>
2022-06-03 05:47:31 +0000

    prints to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\"%s",
+	" - Dora Korpar, 2015-10-19");
 	return (1);
 }
