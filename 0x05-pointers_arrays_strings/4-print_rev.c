ca8e8f929dca2df241ce895afa242e7ada649d77
sualah <sualahabdullai@gmail.com>
2022-06-13 14:23:21 +0000

    print reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - print reverse backward
+ * @s: character string pointer
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (len; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
