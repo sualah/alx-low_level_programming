e247f7f1af8d2886d66f3763623ab2c5aeda8fcb
sualah <sualahabdullai@gmail.com>
2022-06-15 21:47:32 +0000

    capitalize every word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words
+ * @str: char type pointer
+ *
+ * Description: capitalizes all words of a string
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[i] = str[i] - 32;
+		switch (str[i])
+		{
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
