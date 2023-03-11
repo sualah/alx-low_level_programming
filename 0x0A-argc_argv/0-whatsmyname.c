fdf0ddcbc0c65065e3d496390a448e4225092e26
root <sualahabdullai@gmail.com>
2023-03-11 17:06:38 +0000

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 1 if success and 0 otherswise
+ */
 int main(int argc, char **argv)
 {
 	if (argc)
