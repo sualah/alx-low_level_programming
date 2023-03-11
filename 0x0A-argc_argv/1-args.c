dad3c844fbecf3366894820efe20b0a03d600af4
root <sualahabdullai@gmail.com>
2023-03-11 17:04:37 +0000

    argc count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 1 if success and 0 otherswise
+ */
 int main(int argc, char **argv __attribute__((unused)))
 {
-        printf("%d\n", argc - 1);
-        exit(EXIT_SUCCESS);
+	printf("%d\n", argc - 1);
+	exit(EXIT_SUCCESS);
 }
+
