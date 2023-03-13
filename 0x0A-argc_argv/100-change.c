47b4b6b26e9c42f14724af75df1b5617e6ddf704
root <sualahabdullai@gmail.com>
2023-03-13 13:57:03 +0000

    return change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int change(int num);
 
+/**
+ * change - return number of change
+ * @num: number
+ * Return: change
+ */
+int change(int num)
+{
+	int result = 0;
+
+	if (num == 0)
+		return (0);
+	if (num >= 25)
+		return (num / 25 + change(num % 25));
+	if (num >= 10)
+		return (num / 10 + change(num % 10));
+	if (num >= 5)
+		return (num / 5 + change(num % 5));
+	if (num >= 2)
+		return (num / 2 + change(num % 2));
+	if (num >= 1)
+		return (num / 1 + change(num % 1));
+	return (result);
+}
 /**
  * main - entry point
  * @argc: argument count
@@ -17,7 +41,7 @@ int main(int argc, char **argv)
 	{
 		if (atoi(argv[1]) > 0)
 		{
-			result = atoi(argv[1]) / 25;
+			result = change(atoi(argv[1]));
 		}
 	}
 	else
