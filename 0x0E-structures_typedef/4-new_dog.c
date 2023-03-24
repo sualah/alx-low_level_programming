c12decd9cdb9f12a06a2a34dabfc52be2dc26849
root <sualahabdullai@gmail.com>
2023-03-24 11:04:27 +0000

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,19 +12,18 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char n_name = *name;
+	char n_owner = *owner;
 	dog_t *d = malloc(sizeof(*d));
 
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-	else
+	if (d == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	d->name = &n_name;
+	d->age = age;
+	d->owner = &n_owner;
 	free(d);
 	return (d);
 }
