57aac0c9d0d6f074da72ddd0128e62b102c7ee7d
sualah <sabdullai@acsint.io>
2023-06-09 22:26:39 +0000

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: numberi
+ * @argv: string pointer
+ * Return: 1 if failed and 0 otherwise
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
