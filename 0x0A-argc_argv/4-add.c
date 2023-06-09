e3a0dce5c7c614cc94668a4f0df34225da86ab6c
sualah <sabdullai@acsint.io>
2023-06-09 23:07:53 +0000

    add all numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * @argc: numberi
+ * @argv: string pointer
+ * Return: 1 if failed and 0 otherwise
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int result = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (isalpha(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
