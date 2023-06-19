666eb2bcfafb4750a7c18ce547e71aec160dce7c
sualah <sabdullai@acsint.io>
2023-06-19 19:59:39 +0000

    created dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog blueprint
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
