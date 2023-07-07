8722faf2fe6053f9db7d926a6d86785547aa59a5
sualah <sabdullai@acsint.io>
2023-07-07 07:12:55 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/password.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void check_password(char* password) {
+    if (strlen(password) == 4) {
+        if (password[0] + (password[2] << 3) & 0x46C6F48 == 74215240 && password[1] == -1 && password[3] == 0) {
+            // Correct password logic
+        } else {
+            // Incorrect password logic
+        }
+    } else {
+        // Incorrect password length logic
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <password>\n", argv[0]);
+        return 1;
+    }
+
+    check_password(argv[1]);
+
+    return 0;
+}
