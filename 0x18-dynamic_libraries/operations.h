5c76328735cc9da3c678cb9214b7dbc494b0bf1f
root <sualahabdullai@gmail.com>
2023-08-21 21:18:33 +0000

    export c function into python

--- 0x18-dynamic_libraries/operations.h
+++ 0x18-dynamic_libraries/operations.h
@@ -1,11 +1,9 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
-#include <stdio.h>
-#include <stdlib.h>
 
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-int my_div(int a, int b) __attribute__((alias("div")));
+int div(int a, int b);
 int mod(int a, int b);
 #endif
