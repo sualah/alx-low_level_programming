e392ff1f9d6f3ad236af0c8c95e5a374f0972fc3
sualah <sabdullai@acsint.io>
2023-08-21 17:09:59 +0000

    added operation in c

--- 0x18-dynamic_libraries/operations.h
+++ 0x18-dynamic_libraries/operations.h
@@ -1,11 +1,9 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
-#include <stdio.h>
 #include <stdlib.h>
 
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-int div(int a, int b);
 int mod(int a, int b);
 #endif
